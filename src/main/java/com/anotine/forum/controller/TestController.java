e469c7f1b74852dc2093223579263940f51dc7c1
FCBAYAntoine <2215110292@qq.com>
2026-06-20 16:58:25 +0800

    第二次提交 进行了部分环境的准备

---
+++ src/main/java/com/anotine/forum/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.anotine.forum.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
